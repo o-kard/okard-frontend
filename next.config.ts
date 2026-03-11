92939b34593136e36b4ed63bdc5ebbae0aad59af
Prom <prommmm44@gmail.com>
2026-03-11 23:44:22 +0700

    feat: Implement GitHub Actions for UAT PR checks and deployment, and configure Next.js remote image patterns.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '9000',
+        pathname: '/crowdfunding-bucket/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'minio-api.081246.xyz',
+        pathname: '/crowdfunding-bucket/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
